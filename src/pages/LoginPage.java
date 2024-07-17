cce9cc9c029ddcccee9b2cb717e7db2194c30b7a
bsksbr <senthilbdpm@gmail.com>
2024-07-17 21:13:39 +0530

    update/create/delete

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,7 @@
 package pages;
 
 public class LoginPage {
-
+	public static void main(String[] as) {
+		
+	}
 }
---
+++ src/test/HomePageTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class HomePageTest {
+
+	public HomePageTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
